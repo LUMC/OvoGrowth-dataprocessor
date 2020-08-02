7806d5f4fd0a88674e6bdc45b95a3af9784a227f
Dylan Hoogduin <dylanhoogduin@Dylans-MacBook-Pro.local>
2020-08-02 12:41:12 +0200

    test expression 2

--- scripts/insert_expression_to_database.py
+++ scripts/insert_expression_to_database.py
@@ -4,7 +4,7 @@ from helpers.Database import db
 
 def get_cell_ids(cell_markers, reference_id, DB):
     cell_ids = []
-    print(cell_markers[1:10])
+    print(cell_markers[0:10])
     for cell_marker in cell_markers:
         print(reference_id, cell_marker)
         cell_id = DB.connection.execute('select id from cell where cell_marker = "{cell}" and dataset="{reference_id}"'
