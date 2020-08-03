c71a6decbadfd2a788722b08a79754b993025c38
Dylan Hoogduin <dylanhoogduin@Dylans-MacBook-Pro.local>
2020-08-03 14:41:28 +0200

    show error in gene insert 3

--- scripts/insert_genes_to_database.py
+++ scripts/insert_genes_to_database.py
@@ -8,14 +8,13 @@ def insert_genes_to_db(dialect, driver, host, username, password, database, gene
     DB.connection.execute('ALTER TABLE `gene` DISABLE KEYS')
     with open(gene_file) as f:
         for line in f:
-            try:
-                [ensg, symbol, description] = line.replace('\n', '').split(';')
-                description = description.replace('"', "").replace("'", "")
-                symbol = symbol.split(".")[0]
-                gene_values = "()".format()
-                DB.connection.execute("INSERT IGNORE INTO gene (symbol, description) VALUES ('{symbol}', '{desc}')"
-                                      .format(symbol=symbol, desc=description))
-                print("Error in line:\n {line}".format( line=line))
+            [ensg, symbol, description] = line.replace('\n', '').split(';')
+            description = description.replace('"', "").replace("'", "")
+            symbol = symbol.split(".")[0]
+            gene_values = "()".format()
+            DB.connection.execute("INSERT IGNORE INTO gene (symbol, description) VALUES ('{symbol}', '{desc}')"
+                                  .format(symbol=symbol, desc=description))
+            print("Error in line:\n {line}".format( line=line))
     DB.connection.execute('ALTER TABLE `gene` ENABLE KEYS')
     DB.connection.execute('ALTER TABLE `gene_origin` DISABLE KEYS')
     with open(gene_file) as f:
