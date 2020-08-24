02935adb21b3393bed44b00e3175254a6ac079ae
Davy Cats <davycats.dc@gmail.com>
2020-08-24 10:26:16 +0200

    Fix syntax error

--- files/schema.sql
+++ files/schema.sql
@@ -42,7 +42,7 @@ CREATE TABLE `gene` (
   `description` text,
   PRIMARY KEY (`id`),
   UNIQUE KEY `symbol` (`symbol`),
-  INDEX `symbol_index` (`symbol`),
+  INDEX `symbol_index` (`symbol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
